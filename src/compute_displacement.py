1515588b89a95682c93246500edb370a5391925c
Alan Ixba <AAIXBAZZ@GMAIL.COM>
2021-12-21 11:38:55 -0600

    Update compute_displacement.py
    the 'bias mismatch' condition was removed, instead we're using absolute weight values, avoiding inverted maps.

--- src/compute_displacement.py
+++ src/compute_displacement.py
@@ -27,27 +27,20 @@ def normalize(export_path, material):
     for path in glob.glob(export_path, recursive=True):
         # material = path.split(os.path.sep)[-3]
         print('Normalizing displacement map for:', material)
-        # try:
-        disp = torch.tensor(
-            imageio.imread(f"{path}/displacement.jpg").astype(numpy.float32),
-            device="cpu",
-        )
+        try:
+            disp = torch.tensor(
+                imageio.imread(f"{path}/displacement.jpg").astype(numpy.float32),
+                device="cpu",
+            )
+            with torch.no_grad():
+                if disp.ndim == 3:
+                    disp = disp.mean(dim=2)
+                disp = (disp - disp.mean()) / disp.std()
+                print ('Original displacement values:', "Min:", disp.min(), 'Max', disp.max())
 
-        with torch.no_grad():
-            if disp.ndim == 3:
-                disp = disp.mean(dim=2)
-            disp = (disp - disp.mean()) / disp.std()
-            print ('Original displacement values:', "Min:", disp.min(), 'Max', disp.max())
-
-        init_minratio = disp.min() * - 1.0
-        init_maxratio = disp.max()
-
-        if init_minratio > init_maxratio:
-            init_bias = -1
-            print("Bias: Negative")
-        else:
-            init_bias = 1
-            print("Bias: Positive")
+        except (FileNotFoundError, KeyError, ValueError):
+            print('NO DISP. Skipping material.')
+            continue
 
         try:
             bump = torch.tensor(
@@ -61,7 +54,6 @@ def normalize(export_path, material):
         except (FileNotFoundError, KeyError, ValueError):
             print('NO BUMP')
             bump = 0
-
 
         try:
             norm = imageio.imread(f"{path}/normal.jpg")
@@ -78,14 +70,14 @@ def normalize(export_path, material):
 
         with torch.no_grad():
             norm = torch.tensor(norm / 127.5 - 1, device="cpu")[:, :, :3]
-            norm[:, :, 1] = -norm[:, :, 1]
+            # norm[:, :, 1] = -norm[:, :, 1]
 
         print(" - MIN", norm.min(0).values.min(0).values, "MAX", norm.max(0).values.max(0).values)
         print(" - MEAN", norm.mean(dim=(0,1)))
         print(" - NORM", norm.shape, '->', torch.norm(norm[:,:,:3], dim=2, p=2).mean())
 
         weight = torch.tensor(
-            [1 / 32768, 0.001], device="cpu", dtype=torch.float32
+            [1 / 256, 0.001], device="cpu", dtype=torch.float32
         ).requires_grad_()
 
         assert disp.shape[:2] == norm.shape[:2]
@@ -96,9 +88,10 @@ def normalize(export_path, material):
 
         for i in range(15):
             opt.zero_grad()
+
             new_disp = disp * weight[0] + bump * weight[1]
             d_x = new_disp[:, 1:] - new_disp[:, :-1]
-            d_x = -d_x / torch.sqrt(1.0 + d_x ** 2.0)
+            d_x = d_x / torch.sqrt(1.0 + d_x ** 2.0)
             loss_x = F.mse_loss(d_x, norm[:, :-1, 0] * 1.0)
             loss_x.backward()
             del d_x
@@ -110,8 +103,8 @@ def normalize(export_path, material):
 
             loss_y.backward()
             del d_y
-
-            print((loss_x + loss_y).item())
+            # print((loss_x).item(), (loss_y).item())
+            # print((loss_x + loss_y).item())
 
             del new_disp
             opt.step()
@@ -119,35 +112,17 @@ def normalize(export_path, material):
 
         with torch.no_grad():
 
-            k = 32767.0 / 6
+            k = 32767.0 / 2
 
-            print(weight[0].item(), weight[1].item())
-
-            new_disp = disp * weight[0]
-
-            #Compare new bias
-            new_minratio = new_disp.min() * - 1.0
-            new_maxratio = new_disp.max()
-
-            if new_minratio > new_maxratio:
-                new_bias = -1
-                # print("New bias: Negative")
-            else:
-                new_bias = 1
-                # print("New bias: Positive")
-
-            if new_bias != init_bias:
-                new_disp = -new_disp
-                # print("Bias mismatch. Disp inverted!")
-            else:
-                new_disp = new_disp
+            print(weight[0].abs().item(), weight[1].abs().item())
+            new_disp = disp * weight[0].abs()
 
             print("New displacement values for", material, "-","Min:", new_disp.min(), "Max:", new_disp.max())
             computed = 32768.0 + k * new_disp
             assert (computed >= 0).all() and (computed <= 65535).all()
 
             imageio.imwrite(
-                f"{export_path}/disp_new_4k.png",
+                f"{path}/disp_new_4k.png",
                 computed.cpu().numpy().astype(numpy.uint16),
                 format="PNG-FI"
             )
