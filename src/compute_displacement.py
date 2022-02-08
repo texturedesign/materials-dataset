44f8af7bd981c0b38579b3b2a72154f14ac6e064
Alan Ixba <AAIXBAZZ@GMAIL.COM>
2022-02-08 14:36:00 -0600

    Update compute_displacement.py

--- src/compute_displacement.py
+++ src/compute_displacement.py
@@ -1,15 +1,12 @@
 import os
-import glob
 import json
 import random
 import imageio
-import PIL
-from PIL import ImageFilter
-
+import glob
 import numpy
 import torch
 import torch.nn.functional as F
-import torch.optim.lr_scheduler as lr_scheduler
+import torchvision.transforms.functional as TF
 
 from material import Material, MaterialScanner
 from library import MaterialLibrary
@@ -21,7 +18,6 @@ import random
 def normalize(export_path, material):
 
     print('|||NORMALIZING DISPLACEMENT MAPS|||')
-
 
     for path in glob.glob(export_path, recursive=True):
         print('Normalizing displacement map for material', material)
@@ -54,17 +50,12 @@ def normalize(export_path, material):
             print('NO BUMP')
             bump = 0
 
+        ## NORMAL MAP ##
+
         try:
             norm = imageio.imread(f"{path}/normal.jpg")
         except ValueError as exc:
             print('ERROR')
-
-        # NORMAL Pre-processing: Apply blur to normal to decrease complexity in detail.
-        normal_src = PIL.Image.fromarray(norm)
-        normal_src = normal_src.filter(ImageFilter.GaussianBlur(radius=10))
-        data = normal_src.getdata()
-        normal_src = numpy.array(data).reshape(data.size[::-1]+(-1,))
-        norm = normal_src.astype(numpy.float32)
 
         if norm.dtype == numpy.uint16:
             norm = norm.astype(numpy.float32) / 256.0
@@ -75,11 +66,13 @@ def normalize(export_path, material):
 
         with torch.no_grad():
             norm = torch.tensor(norm / 127.5 - 1, device="cpu")[:, :, :3]
-            # norm[:, :, 1] = -norm[:, :, 1]
 
         print(" - MIN", norm.min(0).values.min(0).values, "MAX", norm.max(0).values.max(0).values)
         print(" - MEAN", norm.mean(dim=(0,1)))
         print(" - NORM", norm.shape, '->', torch.norm(norm[:,:,:3], dim=2, p=2).mean())
+
+        norm = TF.gaussian_blur(norm.permute(2, 0, 1), kernel_size=71, sigma=30.0)
+        norm = norm.permute(1, 2, 0)
 
         weight = torch.tensor(
             [1 / 256, 0.001], device="cpu", dtype=torch.float32
@@ -87,11 +80,11 @@ def normalize(export_path, material):
 
         assert disp.shape[:2] == norm.shape[:2]
 
-        lr = 1e-3
+        lr = 5e-4
 
         opt = torch.optim.Adam([weight], lr=lr)
 
-        for i in range(25):
+        for i in range(15):
             opt.zero_grad()
 
             new_disp = disp * weight[0].abs() + bump * weight[1].abs()
@@ -109,14 +102,14 @@ def normalize(export_path, material):
             del d_y
 
             # print((loss_x).item(), (loss_y).item())
-            # print((loss_x + loss_y).item())
+            print((loss_x + loss_y).item())
 
             del new_disp
             opt.step()
 
         with torch.no_grad():
 
-            k = 32767.0 / 2
+            k = 32767.0 / 1.5
 
             print(weight[0].abs().item(), weight[1].abs().item())
             new_disp = disp * weight[0].abs()
@@ -124,7 +117,7 @@ def normalize(export_path, material):
             print("New displacement values for material", material, "-","Min:", new_disp.min(), "Max:", new_disp.max())
             computed = 32768.0 + k * new_disp
             assert (computed >= 0).all() and (computed <= 65535).all()
-
+            print("Saving displacement map...")
             imageio.imwrite(
                 f"{path}/disp_new_4k.png",
                 computed.cpu().numpy().astype(numpy.uint16),
